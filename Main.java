e455b5a74a06088d1a884f45013706e889f41fe8
mayconzanyck <gthubicy@gmail.com>
2025-05-06 15:49:50 -0300

    declaração de variaveis

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+        //VARIAVEIS
+        int minhaIdade = 18;
+        String meuNome = "Maycon";
+
+        Ou usando var:
+        var nomeVariavel = "Maycon";
+         */
+    }
+}
