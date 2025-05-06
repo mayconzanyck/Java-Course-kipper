05aafde9caff9776744e101def41d0af9a1fc628
mayconzanyck <gthubicy@gmail.com>
2025-05-06 15:51:18 -0300

    tipos primitivos

--- Main.java
+++ Main.java
@@ -9,6 +9,35 @@ public class Main {
 
         Ou usando var:
         var nomeVariavel = "Maycon";
-         */
+
+
+        //TIPOS PRIMITIVOS
+
+        numeros inteiros = 100, 583958, 1, 9492
+        byte => 8 bits => -128 a 127
+        short => 16 bits => -32.768 a 32.767
+        int => 32 bits => -2.147.483.648 a 2.147.483.647
+        long => 64 bits => -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807
+
+        numeros decimais = 1.2, 3.4, 7.7
+        float => 32 bits => precisao simples (poucos numeros após virgula, 39.23)
+        double => 64 bits => precisao dupla (mais numeros após virgula, 67.3295)
+
+        String => representar palavras e frases
+
+        char => representar um unico caractere (em aspas simples) => a, b, c, d
+
+        boolean => true ou false
+        */
+
+        byte b = 100;
+        short s = 10000;
+        int i = 100000;
+        long l = 100000L;
+        float f = 10.5f;
+        double d = 20.5;
+        char c = 'A';
+        String str = "Bianca";
+        boolean bool = true;
     }
 }
