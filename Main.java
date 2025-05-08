5e721867cac0f7edc38e625c422fd9e70f1c70de
mayconzanyck <gthubicy@gmail.com>
2025-05-07 21:54:27 -0300

    metodos

--- Main.java
+++ Main.java
@@ -2,20 +2,32 @@
 
 public class Main {
 
+    //Metodo de entrada (Main)
     public static void main(String[] args) {
 
+        //Cada um desses representa uma nova estancia da classe carro
         Carro meuCarro = new Carro("Fusca");
         Carro meuCarro1 = new Carro("BMW");
         Carro meuCarro2 = new Carro("Porsche");
+
+        //Chamando METODO
+        meuCarro.acelerar();
+        meuCarro1.acelerar();
+        meuCarro2.acelerar();
     }
 }
 
-//CONSTRUTOR: atribuir valores(atributos) ao objeto
+//CONSTRUTOR: atribuir variaveis(atributos) ao objeto
 class Carro{
 
     String modelo;
 
     public Carro(String modelo){
         this.modelo = modelo;
+    }
+
+    //METODO(função)
+    public void acelerar(){
+        System.out.println("Acelerando o carro " + this.modelo);
     }
 }
