72be32e0e68cf31d32270519021ee3cfa15e058c
mayconzanyck <gthubicy@gmail.com>
2025-05-07 21:47:02 -0300

    construtores

--- Main.java
+++ Main.java
@@ -1,6 +1,21 @@
+//CLASSES: POO, modelo que devine a estrutura do objeto
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Carro meuCarro = new Carro("Fusca");
+        Carro meuCarro1 = new Carro("BMW");
+        Carro meuCarro2 = new Carro("Porsche");
+    }
+}
+
+//CONSTRUTOR: atribuir valores(atributos) ao objeto
+class Carro{
+
+    String modelo;
+
+    public Carro(String modelo){
+        this.modelo = modelo;
     }
 }
